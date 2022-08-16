40b30329f190f71816b96b1216d5461fa11305f3
TMichael <js5859168@gmail.com>
2022-08-16 12:25:21 +0300

    Homework2

--- app/src/main/java/com/inflames1986/mvplesson/MainActivity.kt
+++ app/src/main/java/com/inflames1986/mvplesson/MainActivity.kt
@@ -1,53 +1,42 @@
 package com.inflames1986.mvplesson
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.github.terrakok.cicerone.androidx.AppNavigator
 import com.inflames1986.mvplesson.databinding.ActivityMainBinding
+import moxy.MvpAppCompatActivity
+import moxy.ktx.moxyPresenter
+import com.inflames1986.mvplesson.interfaces.BackButtonListener
+import com.inflames1986.mvplesson.interfaces.MainView
 
 
-class MainActivity : AppCompatActivity(), MainView {
+class MainActivity : MvpAppCompatActivity(), MainView {
+    val navigator = AppNavigator(this, R.id.container)
+    private val presenter by moxyPresenter { MainPresenter(App.instance.router, AndroidScreens()) }
+    private var vb: ActivityMainBinding? = null
 
-
-    private var _binding: ActivityMainBinding? = null
-    val binding: ActivityMainBinding
-        get() = _binding!!
-
-
-    private lateinit var presenter: CountersPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-
-        setContentView(binding.root)
-
-        initPresenter()
-
-        with(binding) {
-            btnNumber1.setOnClickListener {
-                presenter.onCounterClick(R.id.btnNumber1)
-            }
-            btnNumber2.setOnClickListener {
-                presenter.onCounterClick(R.id.btnNumber2)
-            }
-            btnNumber3.setOnClickListener {
-                presenter.onCounterClick(R.id.btnNumber3)
-            }
-        }
+        vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb?.root)
     }
 
-    private fun initPresenter() {
-        presenter = CountersPresenter(this)
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        App.instance.navigatorHolder.setNavigator(navigator)
     }
 
-    override fun setText(counter: String, position: Int) {
-        with(binding) {
-            when (position) {
-                0 -> tvText1.text = counter
-                1 -> tvText2.text = counter
-                2 -> tvText3.text = counter
+    override fun onPause() {
+        super.onPause()
+        App.instance.navigatorHolder.removeNavigator()
+    }
+
+    override fun onBackPressed() {
+        supportFragmentManager.fragments.forEach {
+            if (it is BackButtonListener && it.backPressed()) {
+                return
             }
         }
+        presenter.backClicked()
     }
 }
